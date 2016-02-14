053934d62c3580e3fb1a8b20f33a9a622de7b70f
Shai <shaibruhis@gmail.com>
2016-02-14 10:32:21 -0800

    Added code that will allow for customization of the marker

--- Spotter/MapViewController.swift
+++ Spotter/MapViewController.swift
@@ -27,17 +27,29 @@ class MapViewController: BaseViewController {
         mapView.mapType = kGMSTypeHybrid
         
         if CLLocationManager.authorizationStatus() == .AuthorizedWhenInUse {
-            
-            locationManager.startUpdatingLocation()
-            // draws a light blue dot where the user is located
-            mapView.myLocationEnabled = true
-            // adds button to bottom right of map that, when tapped, centers on user's location
-            mapView.settings.myLocationButton = true
-        }
+        // Add marker for user's current location
+        let position = CLLocationCoordinate2DMake(-31.868, 151.208) // position will be current location coordinates
+        let usersCurrentLocationMarker = GMSMarker(position: position)
+        usersCurrentLocationMarker.title = "Hello World"    // user's current location pin should have no title?
+//        usersCurrentLocationMarker.icon = UIImage(named: "house")
+        usersCurrentLocationMarker.opacity = 0.9
+        usersCurrentLocationMarker.snippet = "I am currently Selected!"
+        usersCurrentLocationMarker.map = mapView
         
+        // Add parking markers on map
+        // If you are creating several markers with the same image, use the same instance of UIImage for each of the markers. This helps improve the performance of your application when displaying many markers.
+        // This image may have multiple frames. Additionally, the alignmentRectInsets property is respected, which is useful if a marker has a shadow or other unusable region.
+//        for spot in parkingSpotsList {
+//            let  position = CLLocationCoordinate2DMake(-31.868, 151.208) // position will be coordinates in ParkingSpot Model
+//            let marker = GMSMarker(position: position)
+//            marker.title = "Hello World"  // title will be price in ParkingSpot Model
+//            marker.map = mapView
+//        }
         self.view = mapView
+        }
     }
 }
+
 
 // MARK: - CLLocationManagerDelegate
 extension MapViewController: CLLocationManagerDelegate {
