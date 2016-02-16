5c5f6c2ac3fd79d1ab5f89a191b7d4b56093f863
glhchan <glhchan@ucdavis.edu>
2016-02-15 21:42:17 -0800

    removing files for version control

--- Pods/Braintree/BraintreeCard/BTCardClient_Internal.h
+++
@@ -1,8 +0,0 @@
-#import "BTCardClient.h"
-
-@interface BTCardClient ()
-
-/// Exposed for testing to get the instance of BTAPIClient
-@property (nonatomic, strong, readwrite) BTAPIClient *apiClient;
-
-@end
