dff277fb29f6559a8ede2f1eff7d284270288678
Christopher Chan <chris@Christophers-Mac.local>
2016-02-27 17:24:47 -0800

    Price input constraints
    Added constraints to the inputtextfield for seller price edit. Can only
    place 1 decimal, numbers and only max of 2 numbers after a decimal.

--- Spotter/SellerEditPrice.swift
+++ Spotter/SellerEditPrice.swift
@@ -11,17 +11,50 @@ import UIKit
 
 class SellerEditPriceViewController: UIViewController, UITextFieldDelegate{
     @IBOutlet weak var userPriceInput: UITextField!
+    @IBOutlet weak var savePriceButton: UIBarButtonItem!
+    var decimalPointExists: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         userPriceInput.delegate = self
-        userPriceInput.keyboardType = .NumberPad
+        userPriceInput.keyboardType = .DecimalPad
     }
+    
     
     func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
-        print(string)
-        //TODO: constrain to only positive integers and 1 decimal point w max 2 ints after the decimal.
-        return true
+        
+        let validInputSet = NSCharacterSet(charactersInString:"0123456789.").invertedSet
+        let compSepByCharInSet = string.componentsSeparatedByCharactersInSet(validInputSet)
+        let validInputFiltered = compSepByCharInSet.joinWithSeparator("")
+        
+        if(!(textField.text!.characters.contains("."))){
+            decimalPointExists = false
+        }//Accounts for insertion and deletion (primarily deletion); updates whether or not there is a decimal
+        else{
+            var numsAfterDecimal = textField.text?.componentsSeparatedByString(".")
+            if(numsAfterDecimal![1].characters.count < 2 && string != "."){
+                return string == validInputFiltered
+            }
+            else{
+                if(string == ""){
+                    return true
+                }//if we're deleting
+                else{
+                    return false
+                }
+            }
+        }//if there is a decimal, check if entered character is valid and that there isn't more than 2 characters hafter the decimal
+
+        
+        if(string == "."){
+            if(decimalPointExists == true){
+                return false
+            }
+            else{
+                decimalPointExists = true
+            }
+        }//Accounts for single character insertion. If decimal point doesn't exist, allow user to place decimal point. If it does, then don't.
+        return string == validInputFiltered
     }
-    
-}
+}//TODO: show a popup warning notifying that the user is trying to list spot for free (when they input 0., .0, and all other permutations of $0
+
